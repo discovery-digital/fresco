4f037af1c1ddfad61e0dce1d39372a6c5931bf7b
Jon <jonathan_bettger@discovery.com>
2021-11-03 14:42:34 -0400

    Push for releases

--- release.sh
+++ release.sh
@@ -2,4 +2,4 @@
 
 rm -rf $HOME/.gradle/caches
 rm -rf .gradle
-./gradlew clean allclean build uploadArchives
+./gradlew clean allclean build -P repositoryUrl=https://nexus-prod-mercury.dnitv.net/repository/maven-snapshots repositoryUsername=$GITHUB_USERNAME repositoryPassword=$GITHUB_ACCESS_TOKEN uploadArchives
